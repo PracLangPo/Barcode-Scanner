ef5ee6186f4372f3bd61869b152f192864b41264
PracLangPo <93488525+PracLangPo@users.noreply.github.com>
2025-12-19 15:50:08 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,4 @@
-const cacheName = 'inventory-cache-v1';
-const filesToCache = [
-    './inventory.html',
-    './sw.js',
-    './manifest.json',
-    'https://unpkg.com/quagga@0.12.1/dist/quagga.min.js',
-];
-
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(cacheName).then(cache => cache.addAll(filesToCache))
-    );
-});
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
-    );
-});
+const cacheName='inventory-cache-v1';
+const filesToCache=['./index.html','./sw.js','./manifest.json','https://unpkg.com/quagga@0.12.1/dist/quagga.min.js'];
+self.addEventListener('install', e=>{ e.waitUntil(caches.open(cacheName).then(c=>c.addAll(filesToCache)))});
+self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
